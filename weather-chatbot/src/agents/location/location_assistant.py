b0f5b64d8dc7e4debe6ee0f6a0e2f879ff02e14d
Scott Grosenick <scogro@microsoft.com>
2024-05-10 16:50:08 -0700

    Adding geospatial

--- weather-chatbot/src/agents/location/location_assistant.py
+++ weather-chatbot/src/agents/location/location_assistant.py
@@ -1,4 +1,4 @@
 class LocationAssistant:
     """Class for asking user about their location."""
     def invoke(self, message_history: list[dict]) -> str:
-        return "stub for asking user location questions"
+        return "If you provide a location, I can help you find the weather"
