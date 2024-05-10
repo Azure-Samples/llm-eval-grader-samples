599a101541beab0cdee35fbd3eee47a2a57a2f5c
Zikra Toure <zitoure@microsoft.com>
2024-05-10 11:23:12 -0700

    use environ and remove for loop for retry

--- weather-chatbot/src/client/llm_interface.py
+++ weather-chatbot/src/client/llm_interface.py
@@ -18,9 +18,9 @@ class ChatCompletion:
         
         # Set the API key of azure openai
         self.client = AzureOpenAI(
-            api_key=os.getenv("OPENAI_API_KEY"),
-            azure_endpoint=os.getenv("OPENAI_API_BASE"),
-            api_version=os.getenv("OPENAI_API_VERSION")
+            api_key=os.environ["OPENAI_API_KEY"],
+            azure_endpoint=os.environ["OPENAI_API_BASE"],
+            api_version=os.environ["OPENAI_API_VERSION"]
         )
         self.max_retry = os.getenv("MAX_RETRY_COUNT")
         self.deployment_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
@@ -34,33 +34,30 @@ class ChatCompletion:
             max_tokens (Optional[int], optional): temperature parameter for sampling. Defaults to None.
         """
         last_exception = Exception("Invalid state")
-        for i in range(int(self.max_retry)):
-            try:
-        
-                response = self.client.chat.completions.create(
-                    model=self.deployment_name,
-                    messages=messages,
-                    max_tokens=max_tokens,
-                    temperature=temperature,
-                )
-                completion = response.choices[0]
+        try:
+    
+            response = self.client.chat.completions.create(
+                model=self.deployment_name,
+                messages=messages,
+                max_tokens=max_tokens,
+                temperature=temperature,
+            )
+            completion = response.choices[0]
 
-                content = completion.message.content
-                return content
-        
-            except AttributeError as e:
-                last_exception = e
-                print(f"AttributeError, attempt {i + 1}")
-            except RateLimitError as e:
-                last_exception = e
-                print(f"RateLimitError, attempt {i + 1}")
-                if i != self.max_retry - 1:
-                    sleep(6)
-            except (Timeout, ReadTimeoutError, APIConnectionError) as e:
-                last_exception = e
-                e_name = e.__class__.__name__
-                print(f"{e_name}, attempt {i + 1}")
+            content = completion.message.content
+            return content
+    
+        except AttributeError as e:
+            last_exception = e
+            print(f"AttributeError")
+        except RateLimitError as e:
+            last_exception = e
+            print(f"RateLimitError")
+            if i != self.max_retry - 1:
+                sleep(6)
+        except (Timeout, ReadTimeoutError, APIConnectionError) as e:
+            last_exception = e
+            e_name = e.__class__.__name__
+            print(f"{e_name}")
 
         raise last_exception
-
-chat_completion = ChatCompletion()
