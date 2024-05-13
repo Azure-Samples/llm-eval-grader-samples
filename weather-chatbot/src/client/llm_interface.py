f89ad95b862b22a44381e3a61de651a1043f1f1c
Zikra Toure <zitoure@microsoft.com>
2024-05-13 11:03:15 -0700

    remove wrapper

--- weather-chatbot/src/client/llm_interface.py
+++ weather-chatbot/src/client/llm_interface.py
@@ -1,51 +1,34 @@
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 from typing import Optional
-from openai import (
-    RateLimitError,
-    Timeout,
-    APIConnectionError
-)
-from urllib3.exceptions import ReadTimeoutError
-from time import sleep
 import os
 
 
-class ChatCompletion:
-    def __init__(self):
-        """Initialize the ChatCompletion wrapper."""
-        load_dotenv()
-        
-        # Set the API key of azure openai
-        self.client = AzureOpenAI(
-            api_key=os.environ["OPENAI_API_KEY"],
-            azure_endpoint=os.environ["OPENAI_API_BASE"],
-            api_version=os.environ["OPENAI_API_VERSION"]
-        )
-        self.deployment_name = os.environ["OPENAI_DEPLOYMENT_NAME"]
-        
-    def get_completion(self, messages, temperature, max_tokens: Optional[int] = None):
-        """This method generates a response from the Azure OpenAI API
+def get_completion(self, messages, temperature, max_tokens: Optional[int] = None):
+    """This method generates a response from the Azure OpenAI API
 
-        Args:
-            messages: message to send to the Azure OpenAI API
-            temperature: maximum number of tokens to generate
-            max_tokens (Optional[int], optional): temperature parameter for sampling. Defaults to None.
-        """
-        try:
+    Args:
+        messages: message to send to the Azure OpenAI API
+        temperature: maximum number of tokens to generate
+        max_tokens (Optional[int], optional): temperature parameter for sampling. Defaults to None.
+    """
+    load_dotenv()
     
-            response = self.client.chat.completions.create(
-                model=self.deployment_name,
-                messages=messages,
-                max_tokens=max_tokens,
-                temperature=temperature,
-            )
-            completion = response.choices[0]
+    # Set the API key of azure openai
+    client = AzureOpenAI(
+        api_key=os.environ["OPENAI_API_KEY"],
+        azure_endpoint=os.environ["OPENAI_API_BASE"],
+        api_version=os.environ["OPENAI_API_VERSION"]
+    )
+    deployment_name = os.environ["OPENAI_DEPLOYMENT_NAME"]
 
-            content = completion.message.content
-            return content
-    
-        except Exception as e:
-            e_name = e.__class__.__name__
-            print(f"{e_name}")
-            raise e
+    response = client.chat.completions.create(
+        model=deployment_name,
+        messages=messages,
+        max_tokens=max_tokens,
+        temperature=temperature,
+    )
+    completion = response.choices[0]
+
+    content = completion.message.content
+    return content
