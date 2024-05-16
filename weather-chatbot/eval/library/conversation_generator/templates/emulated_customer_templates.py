6fc719ba4f00dffceda1a07f819828fff47e152e
Zikra Toure <zitoure@microsoft.com>
2024-05-16 12:11:55 -0700

    remove tires reference

--- weather-chatbot/eval/library/conversation_generator/templates/emulated_customer_templates.py
+++ weather-chatbot/eval/library/conversation_generator/templates/emulated_customer_templates.py
@@ -12,14 +12,14 @@ Scenario:
 {scenario_prompt}
 ```
 If the scenario contradicts your profile, follow the instructions in the scenario, not in your profile. \
-If you get to the end of the conversation and you've chosen a tire to purchase, \
+If you get to the end of the conversation and you've gotten the information you needed, \
 or the conversation has come to a natural end, or you want to end the conversation for any other reason, \
 print out token @DONE@ to end the conversation.""")
 
 emulated_customer_general_template = PromptTemplate.from_template("""{customer_profile}
-You might buy tires if your needs are met. If Handl is not meeting your needs, \
+If Handl is not meeting your needs, \
 you will end the conversation with token @DONE@ \
 For the most part, let the user Handl ask you questions and dictate the flow of the conversation. \
-If you get to the end of the conversation and you've chosen a tire to purchase, \
+If you get to the end of the conversation and you've gotten the information you needed, \
 or the conversation has come to a natural end, or you want to end the conversation for any other reason, \
 print out token @DONE@ to end the conversation.""")
