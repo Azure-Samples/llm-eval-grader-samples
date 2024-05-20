bbb4b98d8e4780b172f40873a662eb4132e6e67e
Zikra Toure <zitoure@microsoft.com>
2024-05-20 13:33:41 -0700

    modify test

--- weather-chatbot/eval/tests/conversation_generator/test_assistantharness.py
+++ weather-chatbot/eval/tests/conversation_generator/test_assistantharness.py
@@ -8,15 +8,11 @@ class TestAssistantHarness(unittest.TestCase):
     def test_get_reply(self):
         with patch(
                 'src.orchestrator.Orchestrator', autospec=True) \
-                as MockOrchestrator, patch(
-                'eval.library.conversation_generator.assistantHarness.get_first_text_ai_response') \
-                as mock_first_text:
+                as MockOrchestrator:
 
             # Configure the mock behavior
             instance = MockOrchestrator.return_value
-            instance.get_reply.return_value = ['nonsense', 123]
-
-            mock_first_text.return_value = 'orchestrator reply'
+            instance.get_reply.return_value = 'orchestrator reply'
 
             # Configure context
             context = {
@@ -30,13 +26,12 @@ class TestAssistantHarness(unittest.TestCase):
             expected_context = deepcopy(context)
 
             # Run test
-            harness = PromptOrchestratorHarness()
+            harness = OrchestratorHarness()
             reply = harness.get_reply(context=context)
 
             # Context should not change
             self.assertEqual(context, expected_context)
 
-            # reply should be the mock response from get_first_text_ai_response
             self.assertEqual(reply, 'orchestrator reply')
 
     def test_get_reply_none_type_return_from_orchestrator(self):
