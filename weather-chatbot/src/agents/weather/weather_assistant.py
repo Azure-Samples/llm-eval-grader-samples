0d7730dc9c6cca224376a228a2a54859bec44345
Sean Miller <seanmiller168@gmail.com>
2024-05-22 16:39:13 -0700

    initial changes to weather_assistant

--- weather-chatbot/src/agents/weather/weather_assistant.py
+++ weather-chatbot/src/agents/weather/weather_assistant.py
@@ -1,9 +1,18 @@
 from src.context import Context
+from src.clients.weather import Weather, WeatherType
 
 
 class WeatherAssistant:
     """Class for answering weather questions."""
     def invoke(self, context: Context) -> str:
-        # message_history = context.get_messages()
+        message_history = context.get_messages()
+
+        weather_data = None
+
+        if context.weather_category:
+            weather_client = Weather()
+            weather_data = weather_client.get_weather(lat=context.location[0], lon=context.location[1], weather_type=context.weather_category)
+
+
 
         return "weather assistant done (stub)"
